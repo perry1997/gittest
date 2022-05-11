a768cd7a3f0732cd0cafc939ae3fb80e30cb4831
perry <18800138976@163.com>
2022-05-11 22:14:36 +0800

    first commit

---
+++ src/main/java/com/haha/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.haha.git;
+
+/**
+ * @author hahaha
+ * @create 2022051122:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
